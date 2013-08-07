541d2595bef45e8acfb3c686901c7870fd353825
OpenEnergyMonitor <glyn.hudson@openenergymonitor.org>
2013-08-07 11:30:53 +0100

    automatically gets userid, sleep removed

--- convert_to_timestore/export.php
+++ convert_to_timestore/export.php
@@ -1,61 +1,68 @@
 <?php
 
+  error_reporting(E_ALL);
+  ini_set('display_errors', 'on');
+
   $adminkey = '';
 
   require "timestore_class.php";
   $timestore = new Timestore($adminkey);
 
-  $userid = 1;
-  $feedid = 1;
-  $interval = 10;
+  $mysqli = new mysqli("localhost","user","pass","db");
 
-  error_reporting(E_ALL);
-  ini_set('display_errors', 'on');
+  $userlistq = $mysqli->query("SELECT id FROM users WHERE `convert` = '1'");
 
-  $mysqli = new mysqli("localhost","user","pass","database");
+  while ($row = $userlistq->fetch_array())
+  {
+    $userid = $row['id'];
+    echo "USER: $userid\n";
 
-  $feedlistq = $mysqli->query("SELECT id,name FROM feeds WHERE `userid`='$userid' AND `timestore`='0' AND `datatype`='1'");
-  
+  $feedlistq = $mysqli->query("SELECT id,name,`convert` FROM feeds WHERE `userid`='$userid' AND `timestore`='0' AND `datatype`='1'");
+
   while ($row = $feedlistq->fetch_array())
   {
-
-  $feedid = $row['id'];
-
-  $now = time();
-  $start = time()-(3600*24*365*5);
-
-  echo $row['id']." ".$row['name']." Creating timestore node ";
-  print $timestore->create_node($feedid,$interval);
-  echo "ok\n";
-
-  $benchstart = microtime(true);
-
-  do
-  {
-    sleep(1);
-    $result = $mysqli->query("SELECT * FROM feed_$feedid WHERE time>$start ORDER BY time Asc LIMIT 25000");
-    sleep(1);
-    $rows = $result->num_rows;
-
-    if ($rows>0)
+    $feedid = $row['id'];
+    $interval = (int) $row['convert'];
+    if ($interval>0)
     {
-      $csv = array();
-      while($row = $result->fetch_array())
+    $now = time();
+    $start = time()-(3600*24*365*5);
+
+    echo $row['id']." ".$row['name']." Creating timestore node ";
+    print $timestore->create_node($feedid,$interval);
+    echo "ok\n";
+
+    $benchstart = microtime(true);
+
+    do
+    {
+
+      $result = $mysqli->query("SELECT * FROM feed_$feedid WHERE time>$start ORDER BY time Asc LIMIT 100000");
+
+      $rows = $result->num_rows;
+
+      if ($rows>0)
       {
-        $csv[] = $row['time'].",".$row['data'];
-        $start = $row['time'];
+        $csv = array();
+        while($row = $result->fetch_array())
+        {
+          $csv[] = $row['time'].",".$row['data'];
+          $start = $row['time'];
+        }
+        $csv = implode($csv,"\n");
+        $timestore->post_csv($feedid,$csv,null);
+
+        $days = round(($now - $start) / (3600*24));
+
+        echo "Start: ".$start." ".$days." days ".($result->num_rows)."\n";
       }
-      $csv = implode($csv,"\n");
-      $timestore->post_csv($feedid,$csv,null);
+    } while ($rows!=0);
 
-      $days = round(($now - $start) / (3600*24));
+    echo microtime(true)-$benchstart;
 
-      echo "Start: ".$start." ".$days." days ".($result->num_rows)."\n";
-    }
-  } while ($rows!=0);
+    $mysqli->query("UPDATE feeds SET `timestore`='1' WHERE `id`='$feedid'");
+  }
 
-  echo microtime(true)-$benchstart;
-
-  $mysqli->query("UPDATE feeds SET `timestore`='1' WHERE `id`='$feedid'");
+  }
 
   }
