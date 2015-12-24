87e578c2bd41f93598bd9b2bdaac96b4b9150dc8
Trystan Lea <trystan.lea@googlemail.com>
2015-12-24 11:14:06 +0000

    Update emonpi-import.sh

--- emonpi-migrate/emonpi-import.sh
+++ emonpi-migrate/emonpi-import.sh
@@ -10,6 +10,12 @@ mysql -u root -praspberry emoncms < /home/pi/data/emoncms.sql
 mv /home/pi/data/emonhub.conf /home/pi/data/old.emonhub.conf
 cp /home/pi/emonhub/emonhub.conf /home/pi/data/emonhub.conf
 
+mv /home/pi/data/emoncms.conf /home/pi/data/old.emoncms.conf
+
+sudo touch /home/pi/data/emoncms.conf
+sudo chown pi:www-data /home/pi/data/emoncms.conf
+sudo chmod 664 /home/pi/data/emoncms.conf
+
 redis-cli "flushall"
 
 sudo service emonhub start
