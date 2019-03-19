46cc714ae7a8483edfc9db0803a3d4a0d01d44a6
Trystan Lea <trystan.lea@googlemail.com>
2019-03-19 15:03:51 +0000

    Update emonSD_build_test.sh

--- emonSD_build_test.sh
+++ emonSD_build_test.sh
@@ -145,6 +145,17 @@ sudo touch /boot/emonSD-30Oct18
 # !!!! avrdude and emonPiLCD still to install !!!! 
 
 # --------------------------------------------------------------------------------
+# Install log2ram, so that logging is on RAM to reduce SD card wear.
+# Logs are written to disk every hour or at shutdown
+# --------------------------------------------------------------------------------
+curl -Lo log2ram.tar.gz https://github.com/azlux/log2ram/archive/master.tar.gz
+tar xf log2ram.tar.gz
+cd log2ram-master
+chmod +x install.sh && sudo ./install.sh
+cd ..
+rm -r log2ram-master
+
+# --------------------------------------------------------------------------------
 # Manual steps to complete
 # --------------------------------------------------------------------------------
 
