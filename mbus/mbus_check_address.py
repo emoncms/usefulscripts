a45cb5198dc7f7178662045a1a2c3c05c46f9662
TrystanLea <trystan.lea@gmail.com>
2021-12-03 16:17:23 +0000

    minor fix

--- mbus/mbus_check_address.py
+++ mbus/mbus_check_address.py
@@ -1,7 +1,7 @@
 import sys, time
 
 serial_port = input("Please enter serial port (default: /dev/ttyUSB0): ") or "/dev/ttyUSB0"
-baud_rate = int(input("Please enter baud rate (default: /dev/ttyUSB0): ") or "2400")
+baud_rate = int(input("Please enter baud rate (default: 2400): ") or "2400")
 
 print ("Trying to connect on "+str(serial_port)+" "+str(baud_rate))
 
