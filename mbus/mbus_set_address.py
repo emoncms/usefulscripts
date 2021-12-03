1971265757c81f06741d711bb20e4918366953eb
TrystanLea <trystan.lea@gmail.com>
2021-12-03 16:15:33 +0000

    script to check address

--- mbus/mbus_set_address.py
+++ mbus/mbus_set_address.py
@@ -1,7 +1,7 @@
 import sys, time
 
-serial_port = input("Please enter serial port, default: /dev/ttyUSB0:") or "/dev/ttyUSB0"
-baud_rate = int(input("Please enter baud rate, default: /dev/ttyUSB0:") or "2400")
+serial_port = input("Please enter serial port (default: /dev/ttyUSB0): ") or "/dev/ttyUSB0"
+baud_rate = int(input("Please enter baud rate (default: /dev/ttyUSB0): ") or "2400")
 
 print ("Trying to connect on "+str(serial_port)+" "+str(baud_rate))
 
@@ -9,8 +9,8 @@ sys.path.append('/opt/openenergymonitor/emonhub/src')
 from interfacers import *
 mbus = EmonHubMBUSInterfacer.EmonHubMBUSInterfacer("MBUS",serial_port,baud_rate)
 
-old_address = int(input("Please enter current address, default: 254") or "254")
-new_address = int(input("Please enter new address, default: 1") or "1")
+old_address = int(input("Please enter current address (default: 254): ") or "254")
+new_address = int(input("Please enter new address (default: 1): ") or "1")
 
 print ("Sending command to check meter at address "+str(old_address))
 mbus.mbus_short_frame(old_address, 0x40)
