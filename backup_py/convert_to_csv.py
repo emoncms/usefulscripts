ae4cac12f3d1fcaa525099c37ff8eafb8a80c0f5
TrystanLea <trystan.lea@gmail.com>
2022-01-13 15:02:52 +0000

    new scripts to download data from emoncms.org and convert to csv

---
+++ backup_py/convert_to_csv.py
@@ -0,0 +1,56 @@
+import os, json, struct
+from pathlib import Path
+
+username = ""
+
+def phpfina_get_meta(datadir,feedid):
+    if os.path.isfile(datadir+str(feedid)+".meta"):
+        fh = open(datadir+str(feedid)+".meta","rb")
+        tmp = struct.unpack("IIII",fh.read(16))
+        fh.close()
+        meta = {'start_time': tmp[2], 'interval': tmp[3], 'npoints':0}
+        
+        if os.path.isfile(datadir+str(feedid)+".dat"):
+            bytesize = os.stat(datadir+str(feedid)+".dat").st_size
+            meta['npoints'] = int(bytesize/4.0)
+        return meta
+    else:
+        return False
+
+def phpfina_convert(datadir,csvdir,feedid,tag,name):
+    meta = phpfina_get_meta(datadir,feedid)
+    csv_fh = open(csvdir+str(feedid)+"-"+str(tag)+"-"+str(name)+".csv","w")
+    fh = open(datadir+str(feedid)+".dat","rb")
+    for i in range(0,meta['npoints']):
+        time = meta['start_time'] + i*meta['interval']
+        val = struct.unpack("f",fh.read(4))
+        csv_fh.write(str(time)+","+str(val[0])+"\n")
+    fh.close()
+    csv_fh.close()
+    
+def phptimeseries_convert(datadir,csvdir,feedid,tag,name):
+    if os.path.isfile(datadir+"feed_"+str(feedid)+".MYD"):
+        bytesize = os.stat(datadir+"feed_"+str(feedid)+".MYD").st_size
+        npoints = int(bytesize/9.0)
+        
+        csv_fh = open(csvdir+str(feedid)+"-"+str(tag)+"-"+str(name)+".csv","w")
+        fh = open(datadir+"feed_"+str(feedid)+".MYD","rb")
+        
+        for i in range(0,npoints):
+            tmp = struct.unpack('<xIf',fh.read(9))
+            csv_fh.write(str(tmp[0])+","+str(tmp[1])+"\n")
+        fh.close()
+        csv_fh.close()
+
+if not os.path.exists(username+"/csv"):
+    os.mkdir(username+"/csv")
+
+if os.path.exists(username+"/feeds.json"):
+    with open(username+'/feeds.json') as json_file:
+        feeds = json.load(json_file)
+        for f in feeds:
+            if int(f['engine'])==2:
+                phptimeseries_convert(username+"/phptimeseries/",username+"/csv/",f['id'],f['tag'],f['name'])
+            if int(f['engine'])==5:
+                # phpfina_convert(username+"/phpfina/",username+"/csv/",f['id'],f['tag'],f['name'])        
+                pass
---
+++ backup_py/data_downloader.py
@@ -0,0 +1,95 @@
+import requests, os, sys, json, struct
+
+host = "https://emoncms.org"
+username = ""
+password = ""
+
+def phpfina_get_meta(datadir,feedid):
+    if os.path.isfile(datadir+str(feedid)+".meta"):
+        fh = open(datadir+str(feedid)+".meta","rb")
+        tmp = struct.unpack("IIII",fh.read(16))
+        fh.close()
+        meta = {'start_time': tmp[2], 'interval': tmp[3], 'npoints':0}
+        
+        if os.path.isfile(datadir+str(feedid)+".dat"):
+            bytesize = os.stat(datadir+str(feedid)+".dat").st_size
+            meta['npoints'] = int(bytesize/4.0)
+        return meta
+    else:
+        return False
+
+def phpfina_create_meta(datadir,feedid,meta):
+    fh = open(datadir+str(feedid)+".meta","wb")
+    fh.write(struct.pack("I",0))
+    fh.write(struct.pack("I",0))
+    fh.write(struct.pack("I",meta['start_time']))
+    fh.write(struct.pack("I",meta['interval']))
+    fh.close()
+
+def download_file(filename,url,mode):
+    size = 0
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(filename, mode) as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                f.write(chunk)
+                size += len(chunk)
+    return size
+
+def phpfina_download(datadir,feedid,host,apikey):
+    # 1. Get remote meta
+    result = requests.get(host+"/feed/getmeta.json",params={'id':feedid,'apikey':apikey})
+    meta = json.loads(result.text)
+
+    # 2. Get local meta
+    download_start = 0
+    local_meta = phpfina_get_meta(datadir,feedid)
+    if local_meta:
+        if meta['start_time']!=local_meta['start_time']:
+            print("Error: local phpfina meta start_time does not match remote")
+            return False
+        if meta['interval']!=local_meta['interval']:
+            print("Error: local phpfina meta interval does not match remote")
+            return False
+        download_start = local_meta['npoints']*4
+
+    # 3. Create meta file
+    phpfina_create_meta(datadir,feedid,meta)
+    # 4. Download dat file
+    download_size = download_file(datadir+str(feedid)+".dat",host+"/feed/export.json?id="+str(feedid)+"&start="+str(download_start)+"&apikey="+apikey,'ab')
+    print ("PHPFina id="+str(feedid)+" downloaded: "+str(download_size)+" bytes")
+
+def phptimeseries_download(datadir,feedid,host,apikey):
+    download_start = 0
+    if os.path.isfile(datadir+"feed_"+str(feedid)+".MYD"):
+        download_start = os.stat(datadir+"feed_"+str(feedid)+".MYD").st_size
+    download_size = download_file(datadir+"feed_"+str(feedid)+".MYD",host+"/feed/export.json?id="+str(feedid)+"&start="+str(download_start)+"&apikey="+apikey,'ab')
+    print ("PHPTimeSeries id="+str(feedid)+" downloaded: "+str(download_size)+" bytes")
+
+# -------------------------------
+
+result = requests.post(host+"/user/auth.json",data={'username':username,'password':password})
+user_auth = json.loads(result.text)
+apikey = user_auth['apikey_read']
+
+if not os.path.exists(username):
+    os.mkdir(username)
+
+if not os.path.exists(username+"/phpfina"):
+    os.mkdir(username+"/phpfina")
+    
+if not os.path.exists(username+"/phptimeseries"):
+    os.mkdir(username+"/phptimeseries")
+    
+result = requests.get(host+"/feed/list.json",params={'apikey':apikey})
+feeds = json.loads(result.text)
+
+fh = open(username+"/feeds.json","w")
+fh.write(json.dumps(feeds))
+fh.close()
+ 
+for f in feeds:
+    if int(f['engine'])==2:
+        phptimeseries_download(username+"/phptimeseries/",f['id'],host,apikey) 
+    if int(f['engine'])==5:
+        phpfina_download(username+"/phpfina/",f['id'],host,apikey)
