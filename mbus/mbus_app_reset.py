b447cceb34efc74369a86e037913de502049c38e
TrystanLea <trystan.lea@gmail.com>
2023-03-15 14:30:03 +0000

    application reset

---
+++ mbus/mbus_app_reset.py
@@ -0,0 +1,14 @@
+import sys, time
+
+serial_port = input("Please enter serial port (default: /dev/ttyUSB0): ") or "/dev/ttyUSB0"
+baud_rate = int(input("Please enter baud rate (default: 2400): ") or "2400")
+
+print ("Trying to connect on "+str(serial_port)+" "+str(baud_rate))
+
+sys.path.append('/opt/openenergymonitor/emonhub/src')
+from interfacers import *
+mbus = EmonHubMBUSInterfacer.EmonHubMBUSInterfacer("MBUS",serial_port,baud_rate)
+
+address = int(input("Please enter address to check (default: 1): ") or "1")
+
+mbus.mbus_application_reset(address)
